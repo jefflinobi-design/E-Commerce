095daff0c7af826bf570f34617b834ed02dce7c9
jefflinobi-design <jefflinobi@gmail.com>
2026-03-24 21:25:15 +0530

    final fix for vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import ProductCard from "../Components/ProductCard";
+import ProductCard from "../Components/ProductCard.jsx";
 
 
 const Home = ({ search }) => {
